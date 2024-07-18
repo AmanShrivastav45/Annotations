e077279b8180ac2f1583cd201786a91d87f6d0bb
Aman-Shrivastav <amanshrivastav4501@gmail.com>
2024-07-19 03:10:25 +0530

    Minor bug fixes

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Astronaut from "../style/Astronaut";
+
+const Landing = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <Astronaut />
+    </div>
+  );
+};
+
+export default Landing;
