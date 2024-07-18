b2e46fc12fba02fa8ef8adaa79baeb8506a81ef0
Aman-Shrivastav <amanshrivastav4501@gmail.com>
2024-07-19 03:24:19 +0530

    Frontend routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "../src/pages/Home.jsx";
 import Login from "../src/pages/Login.jsx";
 import Signup from "../src/pages/Signup.jsx";
 import AddEditNotes from "./components/AddEditNotes.jsx";
+import Landing from "./pages/Landing.jsx";
 
 function App() {
   return (
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Astronaut from "../style/Astronaut";
+import Loader from "../components/Loader";
 
 const Landing = () => {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <Astronaut />
+    <div className="h-screen w-screen flex items-start justify-center bg-[#2a2a2a]">
+      <Loader/>
+      <h1 className="Geist text-white text-2xl mt-24 ">Welcome to Annotations!</h1>
     </div>
   );
 };
