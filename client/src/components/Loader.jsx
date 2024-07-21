5392d33244f05ad07eba528080f35caad995ac51
Aman-Shrivastav <amanshrivastav4501@gmail.com>
2024-07-21 11:15:44 +0530

    loading sequences updated

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div style={{zIndex:10000}} className="fixed z-10 bg-[#0a0a0b] bg-opacity-30 top-0 left-0 w-full h-screen flex items-center justify-center">
+    <div style={{zIndex:10000}} className="fixed z-10 bg-[#0a0a0b] bg-opacity-80 top-0 left-0 w-full h-screen flex items-center justify-center">
       <div className="spinner-box">
         <div className="blue-orbit leo"></div>
         <div className="green-orbit leo"></div>
