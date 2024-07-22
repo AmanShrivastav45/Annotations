5718c5cfba8c9f3dcd4b1e03c1d32a6500093af3
Aman Shrivastav <amanshrivastav4501@gmail.com>
2024-07-22 13:30:41 +0530

    Update AddEditNotes.jsx

--- client/src/components/AddEditNotes.jsx
+++ client/src/components/AddEditNotes.jsx
@@ -76,7 +76,7 @@ const AddEditNotes = ({ noteData, type, onClose, getAllNotes }) => {
   };
 
   return (
-    <div  style={{ zIndex: 1000 }} className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-75 flex items-center justify-center">
+    <div  style={{ zIndex: 1000 }} className="fixed top-0 left-0 h-full w-full bg-black bg-opacity-75 flex items-center justify-center">
       <div className="relative w-[95%] 2xl:w-[1440px] p-4 md:px-6 border border-[#2a2a2a] bg-opacity-75 bg-black rounded-[10px] flex flex-col items-center">
         <button
           onClick={onClose}
